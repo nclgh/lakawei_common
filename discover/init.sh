72fddc7af7add5d3f2325633e0e2f753ce072803
nichiluan <nichiluan@bytedance.com>
2019-03-22 18:59:40 +0800

    feat: add conf
    Change-Id: I204963b0b300302fb80418c0bf4738c6eb0be727

---
+++ discover/init.sh
@@ -0,0 +1,3 @@
+rm -rf /opt/lakawei/conf/discover/*
+mkdir -p /opt/lakawei/conf/discover/
+cp ./config.yml /opt/lakawei/conf/discover/
---
+++ init.sh
@@ -0,0 +1,3 @@
+rm -rf /opt/lakawei/conf/
+mkdir -p /opt/lakawei/conf/
+cp -r ./* /opt/lakawei/conf/
